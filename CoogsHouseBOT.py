1e7201d9ca0ba5b9531be0d86da4761c1901f890
Nguyen Doan <colnguyenwinn@yahoo.com>
2019-02-06 22:00:56 -0500

    Small syntax fix to regex

--- CoogsHouseBOT.py
+++ CoogsHouseBOT.py
@@ -12,7 +12,7 @@ subreddit = reddit.subreddit("UniversityOfHouston")
 for submission in subreddit.hot(limit = 10)
 	comments = reddit.get_comments('UniversityOfHouston')
 	for comment in comments:
-		match = re.search("^[Ww]+[Hh]+[Oo]+[Ss]+[Ee]*+[ ]+[Hh][Oo]+[Uu]+[Ss]+[Ee]+", comment)
+		match = re.search("(^[Ww]+[Hh]+[Oo]+[Ss]+[Ee]*+[ ]+[Hh][Oo]+[Uu]+[Ss]+[Ee]+)", comment)
 		if match
 			print("COOOOGS HOUSE!!")
 	time.sleep(480)
